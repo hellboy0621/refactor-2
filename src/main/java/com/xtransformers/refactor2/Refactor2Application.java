e3e7ae1c05ae5302bb45428019392c4b5433b3ad
hellboy0621 <hero0621@126.com>
2021-09-03 21:58:56 +0800

    init

---
+++ src/main/java/com/xtransformers/refactor2/Refactor2Application.java
@@ -0,0 +1,13 @@
+package com.xtransformers.refactor2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Refactor2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Refactor2Application.class, args);
+    }
+
+}
