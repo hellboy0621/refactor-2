0f7714eac86cd68d837a4d2b0f8ed0790223faa8
hellboy0621 <hero0621@126.com>
2021-09-05 21:23:12 +0800

    feat: 新增HTML版本的对账单及其单测

--- src/main/java/com/xtransformers/refactor2/service/StatementService.java
+++ src/main/java/com/xtransformers/refactor2/service/StatementService.java
@@ -20,6 +20,24 @@ public class StatementService {
         return renderPlainText(statementDataService.createStatementData(invoice, plays));
     }
 
+    public String htmlStatement(Invoice invoice, Map<String, Play> plays) throws Exception {
+        return renderHtml(statementDataService.createStatementData(invoice, plays));
+    }
+
+    private String renderHtml(StatementData data) {
+        String result = "<h1>Statement for " + data.getCustomer() + "</h1>\n";
+        result += "<table>\n";
+        result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+        for (Performance perf : data.getPerformances()) {
+            result += " <tr><td>" + perf.getPlay().getName() + "</td><td>" + perf.getAudience() + "</td>";
+            result += "<td>" + FormatUtil.usd(perf.getAmount() / 100) + "</td></tr>\n";
+        }
+        result += "</table>\n";
+        result += "<p>Amount owed is <em>" + FormatUtil.usd(data.getTotalAmount() / 100) + "</em></p>\n";
+        result += "<p>You earned <em>" + data.getTotalVolumeCredits() + "</em> credits</p>\n";
+        return result;
+    }
+
     private String renderPlainText(StatementData statementData) {
         String result = "Statement for " + statementData.getCustomer() + "\n";
         for (Performance perf : statementData.getPerformances()) {
--- src/test/java/com/xtransformers/refactor2/service/StatementServiceTest.java
+++ src/test/java/com/xtransformers/refactor2/service/StatementServiceTest.java
@@ -62,4 +62,22 @@ public class StatementServiceTest {
         assertEquals(expected, statement);
     }
 
+    @Test
+    public void testHtmlStatement() throws Exception {
+        Invoice invoice = JSONObject.parseObject(JsonConstant.INVOICES, new TypeReference<List<Invoice>>() {
+        }).get(0);
+        Map<String, Play> plays = JSONObject.parseObject(JsonConstant.PLAYS, new TypeReference<Map<String, Play>>() {
+        });
+        String statement = new StatementService().htmlStatement(invoice, plays);
+        String expected = "<h1>Statement for BigCo</h1>\n" +
+                "<table>\n" +
+                "<tr><th>play</th><th>seats</th><th>cost</th></tr> <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n" +
+                " <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n" +
+                " <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n" +
+                "</table>\n" +
+                "<p>Amount owed is <em>$1,730.00</em></p>\n" +
+                "<p>You earned <em>47</em> credits</p>\n";
+        assertEquals(expected, statement);
+    }
+
 }
