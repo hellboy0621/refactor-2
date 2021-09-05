57e9a855149bac5ffd895bbc51fc8ac55ee76689
hellboy0621 <hero0621@126.com>
2021-09-05 18:07:00 +0800

    refactor: 拆分计算阶段与格式化阶段 - 把方法拆分到不同的类中

--- src/main/java/com/xtransformers/refactor2/service/StatementService.java
+++ src/main/java/com/xtransformers/refactor2/service/StatementService.java
@@ -4,12 +4,8 @@ import com.xtransformers.refactor2.domain.Invoice;
 import com.xtransformers.refactor2.domain.Performance;
 import com.xtransformers.refactor2.domain.Play;
 import com.xtransformers.refactor2.domain.StatementData;
-import org.springframework.beans.BeanUtils;
+import com.xtransformers.refactor2.util.FormatUtil;
 
-import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -18,93 +14,19 @@ import java.util.Map;
  */
 public class StatementService {
 
+    private StatementDataService statementDataService = new StatementDataService();
+
     public String statement(Invoice invoice, Map<String, Play> plays) throws Exception {
-        return renderPlainText(createStatementData(invoice, plays));
-    }
-
-    private StatementData createStatementData(Invoice invoice, Map<String, Play> plays) throws Exception {
-        StatementData statementData = new StatementData();
-        statementData.setCustomer(invoice.getCustomer());
-        List<Performance> collect = new ArrayList<>();
-        for (Performance each : invoice.getPerformances()) {
-            collect.add(enrichPerformance(each, plays));
-        }
-        statementData.setPerformances(collect);
-
-        statementData.setTotalAmount(totalAmount(statementData));
-        statementData.setTotalVolumeCredits(totalVolumeCredits(statementData));
-        return statementData;
-    }
-
-    private Performance enrichPerformance(Performance performance, Map<String, Play> plays) throws Exception {
-        Performance result = new Performance();
-        BeanUtils.copyProperties(performance, result);
-        result.setPlay(playFor(result, plays));
-        result.setAmount(amountFor(result));
-        result.setVolumeCredits(volumeCreditsFor(result));
-        return result;
+        return renderPlainText(statementDataService.createStatementData(invoice, plays));
     }
 
     private String renderPlainText(StatementData statementData) {
         String result = "Statement for " + statementData.getCustomer() + "\n";
         for (Performance perf : statementData.getPerformances()) {
-            result += "  " + perf.getPlay().getName() + ": " + usd(perf.getAmount() / 100) + " (" + perf.getAudience() + " seats)\n";
+            result += "  " + perf.getPlay().getName() + ": " + FormatUtil.usd(perf.getAmount() / 100) + " (" + perf.getAudience() + " seats)\n";
         }
-        result += "Amount owed is " + usd(statementData.getTotalAmount() / 100) + "\n";
+        result += "Amount owed is " + FormatUtil.usd(statementData.getTotalAmount() / 100) + "\n";
         result += "You earned " + statementData.getTotalVolumeCredits() + " credits\n";
-        return result;
-    }
-
-    private int totalAmount(StatementData statementData) {
-        return statementData.getPerformances()
-                .stream()
-                .mapToInt(Performance::getAmount)
-                .sum();
-    }
-
-    private int totalVolumeCredits(StatementData statementData) {
-        return statementData.getPerformances()
-                .stream()
-                .mapToInt(Performance::getVolumeCredits)
-                .sum();
-    }
-
-    private String usd(int aNumber) {
-        return NumberFormat.getCurrencyInstance(Locale.US).format(aNumber);
-    }
-
-    private int volumeCreditsFor(Performance aPerformance) {
-        int result = 0;
-        result += Math.max(aPerformance.getAudience() - 30, 0);
-        if ("comedy".equals(aPerformance.getPlay().getType())) {
-            result += Math.floor(aPerformance.getAudience() / 5);
-        }
-        return result;
-    }
-
-    private Play playFor(Performance aPerformance, Map<String, Play> plays) {
-        return plays.get(aPerformance.getPlayId());
-    }
-
-    private int amountFor(Performance aPerformance) throws Exception {
-        int result = 0;
-        switch (aPerformance.getPlay().getType()) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.getAudience() > 30) {
-                    result += 1000 * (aPerformance.getAudience() - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.getAudience() > 20) {
-                    result += 10000 + 500 * (aPerformance.getAudience() - 20);
-                }
-                result += 300 * aPerformance.getAudience();
-                break;
-            default:
-                throw new Exception("unknown type: " + aPerformance.getPlay().getType());
-        }
         return result;
     }
 
